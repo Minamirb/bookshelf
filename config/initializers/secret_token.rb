37275ca76686dd04f6480af27ce4dcc12f9b23e8
Satochika daimon <s.daimon100@gmail.com>
2012-05-19 11:54:19 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booklist::Application.config.secret_token = '6e680e6da82100ebafa077de36eff3ee56f19511ca3d359312eb5c7f7ea8a48b8635802ee8c10d94ecfd9a94a2c619723659c18e76b74cabcf10e6190297821e'
